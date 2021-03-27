4906a77dcf9faee82b93917b43eac17adf3caeeb
Matthew <pinoycc05@aol.com>
2021-03-27 16:22:46 -0700

    intial fectch api code in JS

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,43 @@
 
+
+// create formSubmitHandler 
+
+// fetch yelp API for coffee roasterie locations 
+
+var getShopLocations = function() {
+    var apiUrl = "httpsforcoffeeroasteries" + shops + "httpsforlocations";
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+
+
+// fetch google places API for nearest coffee roasterie location 
+
+var getShopRadius = function() {
+    var apiUrl = "httpsforcoffeeroasteries" + shops + "httpsforlocations";
+    fetch(apiUrl).then(function(response) {
+        response.json().then(function(data) {
+            console.log(data);
+        });
+    });
+};
+
+// create function to display top 5 coffee roasteries 
+
+// create for loop over arrays
+    // for(var i = 0; i < shop.length; i++) {
+
+    //}
+    // create a span element to hold coffee shop name
+    //append to the container 
+    //append container to the DOM
+
+// display map with nearest coffee roasterie locations
+
+// create addEventListenrer to form container 
+
+
