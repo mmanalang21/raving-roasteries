5f59a3ef0a345dbf2ec0e8726baa46b6d0df99c6
Matthew <pinoycc05@aol.com>
2021-03-27 19:43:42 -0700

    updated JS and fetch API code

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
 // create variables to store form elements
-    // var userFormEl = document.querySelector("#user-form");
-    // var zipcodeInputEl = document.querySelector("#zipcode");
+var userFormEl = document.querySelector("#user-form");
+var zipcodeInputEl = document.querySelector("#zipcode");
 
 // create formSubmitHandler 
+var formSubmitHandler = function() {
+
+}
 
 // fetch yelp API for coffee roasterie locations 
 
 var getShopLocations = function(zipcode) {
-    var apiUrl = "https://api.yelp.com/v3/businesses/search?term=coffee&location=91105"
+    var apiUrl = "https://api.yelp.com/v3/businesses/search?term=coffee&location=91105" + zi;
     fetch(apiUrl).then(function(response) {
         response.json().then(function(data) {
             console.log(data);
@@ -41,12 +44,15 @@ var getShopLocations = function(zipcode) {
     //}
 
     // create a span element to hold coffee shop name
+    var nameEl = document.createElement("span");
+    nameEl.textContent = shopName;
+    
     //append to the container 
+    shopEl.appendChild(nameEl);
+    
     //append container to the DOM
 
 // display map with nearest coffee roasterie locations
 
 // create addEventListenrer to form container 
-
-
-getShopLocations();
+userFormEl.addEventListener("submit", formSubmitHandler);
